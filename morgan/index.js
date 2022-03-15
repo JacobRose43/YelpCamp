23848e718fed0da0074ece6fb609fabbe35fe9b1
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-03-15 22:19:02 +0100

    update

--- morgan/index.js
+++ morgan/index.js
@@ -5,6 +5,16 @@ const port = 8080;
 
 app.use(morgan('tiny'));
 
+app.use((req, res, next) => {
+	req.requestTime = Date.now();
+	console.log(req.method, req.originalUrl, req.hostname);
+	next();
+});
+
+app.use((req, res) => {
+	res.status(404).send('NOT FOUND');
+});
+
 app.get('/', (req, res) => {
 	res.send('homepage');
 });
