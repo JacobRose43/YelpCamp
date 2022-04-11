e81555bf39986d7d2c54882cd0fda6a265082611
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-04-11 19:26:19 +0200

    update

--- other/myApp/index.js
+++ other/myApp/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
 const app = express();
 const port = 8080;
 const path = require('path');
+const session = require('express-session');
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 const ejsMate = require('ejs-mate');
+const User = require('./models/user');
 
 mongoose.connect('mongodb://localhost:27017/nodeApp');
 
@@ -18,12 +19,39 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(session({ secret: 'blockofsecret', resave: true, saveUninitialized: true }));
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
 	res.render('home');
 });
 
 app.get('/login', (req, res) => {
 	res.render('login');
+});
+
+app.post('/login', async (req, res) => {
+	const { password, username } = req.body;
+	const foundUser = await User.findAndValidate(username, password);
+	if (foundUser) {
+		req.session.user_id = User._id;
+		req.session.loggedin = true;
+		res.send(`welcome ${username}`);
+	} else {
+		res.redirect('/login');
+	}
+});
+
+app.get('/signup', (req, res) => {
+	res.render('signup');
+});
+
+app.post('/signup', async (req, res) => {
+	const { password, username } = req.body;
+	const user = new User({ username, password });
+	await user.save();
+	req.session.user_id = User._id;
+	res.redirect('/');
 });
 
 app.listen(port, () => {
