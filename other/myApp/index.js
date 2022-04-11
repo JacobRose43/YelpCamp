30ddf96b74dbc56a6be9003103bc358181babb0c
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-04-11 18:32:02 +0200

    update

--- other/myApp/index.js
+++ other/myApp/index.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const db = mongoose.connection;
 const ejsMate = require('ejs-mate');
 
-mongoose.connect('mongodb://localhost:27017/car');
+mongoose.connect('mongodb://localhost:27017/nodeApp');
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
@@ -20,6 +20,10 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
 	res.render('home');
+});
+
+app.get('/login', (req, res) => {
+	res.render('login');
 });
 
 app.listen(port, () => {
